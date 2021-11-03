3d24bd0bcb716d5d40a0db8077a5e8e19152cd29
gessi <gessilane.i3solucoes@gmail.com>
2021-11-03 18:13:13 -0300

    projeto finalizado

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,62 @@
+class ContactsController < ApplicationController
+  
+  before_action :require_logged_in_user
+  before_action :set_contact, only: %i[ show edit update destroy ]
+
+  def index
+    @contacts = current_user.contacts
+  end
+
+  def show
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def edit
+  end
+
+  def create
+    @contact = current_user.contacts.build(contact_params)
+
+    respond_to do |format|
+      if @contact.save
+        format.html { redirect_to @contact, notice: "Contact was successfully created." }
+        format.json { render :show, status: :created, location: @contact }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @contact.update(contact_params)
+        format.html { redirect_to @contact, notice: "Contact was successfully updated." }
+        format.json { render :show, status: :ok, location: @contact }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @contact.destroy
+    respond_to do |format|
+      format.html { redirect_to contacts_url, notice: "Contact was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_contact
+      @contact = current_user.contacts.find(params[:id])
+    end
+
+    def contact_params
+      params.require(:contact).permit(:name, :phone)
+    end
+end
